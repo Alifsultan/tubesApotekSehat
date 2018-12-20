18295622ead8e4dc05851cde78bbb66fe0d7dd2d
Alif Mohammad Sultan Saifullah <43581045+Alifsultan@users.noreply.github.com>
2018-12-20 22:52:14 +0700

    membuat class koneksi database

--- connect.java
+++ connect.java
@@ -14,9 +14,9 @@ public class connect {
     public static Statement stm;
     public static void main(String args[]){
         try {
-            String url ="jdbc:mysql://localhost/tutorial";
-            String user="root";
-            String pass="";
+            String url ="jdbc:mysql://localhost/from_login";
+            String user="admin";
+            String pass="123";
             Class.forName("com.mysql.jdbc.Driver");
             con =DriverManager.getConnection(url,user,pass);
             stm = con.createStatement();
